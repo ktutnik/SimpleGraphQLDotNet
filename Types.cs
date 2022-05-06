3e3788512ad288cbe9c1d1f8719aa3c4d76ce4f4
I Ketut Sandiarsa <ktutnik@gmail.com>
2022-05-06 15:12:38 +0800

    Added query with object parameter

--- Types.cs
+++ Types.cs
@@ -6,6 +6,22 @@ public class Note
     public string Message { get; set; }
 }
 
+public class Search
+{
+    public int Offset { get; set; }
+    public int Limit { get; set; }
+}
+
+public class SearchInputType: InputObjectGraphType<Search> 
+{
+    public SearchInputType()
+    {
+        Name = "Search";
+        Field(c => c.Limit);
+        Field(c => c.Offset);
+    }
+}
+
 public class NoteType : ObjectGraphType<Note>
 {
     public NoteType()
@@ -21,10 +37,12 @@ public class NotesQuery : ObjectGraphType
 {
     public NotesQuery()
     {
-        Field<ListGraphType<NoteType>>("notes", resolve: context => new List<Note> {
-      new Note { Id = Guid.NewGuid(), Message = "Hello World!" },
-      new Note { Id = Guid.NewGuid(), Message = "Hello World! How are you?" }
-    });
+        Field<ListGraphType<NoteType>>("notes",
+            arguments: new QueryArguments(new QueryArgument<SearchInputType>() { Name = "search" }),
+            resolve: context => new List<Note> {
+                new Note { Id = Guid.NewGuid(), Message = "Hello World!" },
+                new Note { Id = Guid.NewGuid(), Message = "Hello World! How are you?" }
+            });
     }
 }
 
